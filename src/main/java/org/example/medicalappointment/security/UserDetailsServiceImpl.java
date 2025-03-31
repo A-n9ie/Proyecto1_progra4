0932f5c9dc4a5cabfa8f75e7aa6cac2a016c6cba
IvanCio21 <ivan.espinoza.mora@est.una.ac.cr>
2025-03-31 02:44:34 -0600

    AAprobado o no falta mensaje que avise

--- src/main/java/org/example/medicalappointment/security/UserDetailsServiceImpl.java
+++ src/main/java/org/example/medicalappointment/security/UserDetailsServiceImpl.java
@@ -5,8 +5,10 @@ import org.example.medicalappointment.logic.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.*;
-
-
+import org.hibernate.Hibernate;
+import org.springframework.transaction.annotation.Transactional;
+import org.example.medicalappointment.logic.Medico;
+import org.example.medicalappointment.data.DoctorRepository;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,7 +19,11 @@ import org.springframework.stereotype.Service;
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UsuarioRepository usuarioRepository;
-/*
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    /*
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try{
@@ -32,19 +38,31 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         }
     }
  */
-        @Override
-        public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-            System.out.println("Cargando usuario: " + username);
-            Usuario user = usuarioRepository.findByUsuario(username);
-            try {
-                if (user == null) {
-                    System.out.println("Usuario no encontrado: " + username);
-                    throw new UsernameNotFoundException("Username " + username + " not found");
-                }
-                return new UserDetailsImp(user);
-            } catch (Exception e) {
-                System.out.println("Error al cargar usuario: " + username);
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        System.out.println("Cargando usuario: " + username);
+        Usuario user = usuarioRepository.findByUsuario(username);
+        Medico medico = doctorRepository.findByUsuario(user);
+        System.out.println(medico.getAprobado());
+
+        try {
+            if (user == null) {
+                System.out.println("Usuario no encontrado: " + username);
                 throw new UsernameNotFoundException("Username " + username + " not found");
             }
+
+            if (!medico.getAprobado()) {
+                throw new UsernameNotFoundException("Su usuario no está aprobado, comuníquese con el administrador");
+            }
+            return new UserDetailsImp(user);
+
+        } catch (Exception e) {
+            System.out.println("Error al cargar usuario: " + username);
+            throw new UsernameNotFoundException("Username " + username + " not found");
         }
     }
+
+
+}
+
